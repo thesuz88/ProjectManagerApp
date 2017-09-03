df98ac11ceac1eee77d3388e0a93d90eac583571
thesuz88 <stevensuzio@gmail.com>
2017-09-03 13:21:36 -0400

    outlined DAO interface

--- src/main/java/com/gc/spring/DAO/Dao.java
+++ src/main/java/com/gc/spring/DAO/Dao.java
@@ -1,4 +1,20 @@
 package com.gc.spring.DAO;
 
+import com.gc.spring.tables.UsersEntity;
+
+import java.util.List;
+
 public interface Dao {
+
+    List<UsersEntity> findAll();
+
+    List<UsersEntity> findById();
+
+    List<UsersEntity> findByUsername();
+
+    boolean insertUser(UsersEntity user);
+
+    boolean updateUser(UsersEntity user);
+
+    boolean deleteUser(UsersEntity user);
 }
--- src/main/java/com/gc/spring/DAO/HibernateDao.java
+++ src/main/java/com/gc/spring/DAO/HibernateDao.java
@@ -1,4 +1,33 @@
 package com.gc.spring.DAO;
 
+import com.gc.spring.tables.UsersEntity;
+
+import java.util.List;
+
 public class HibernateDao implements Dao {
+
+
+    public List<UsersEntity> findAll() {
+        return null;
+    }
+
+    public List<UsersEntity> findById() {
+        return null;
+    }
+
+    public List<UsersEntity> findByUsername() {
+        return null;
+    }
+
+    public boolean insertUser(UsersEntity user) {
+        return false;
+    }
+
+    public boolean updateUser(UsersEntity user) {
+        return false;
+    }
+
+    public boolean deleteUser(UsersEntity user) {
+        return false;
+    }
 }
